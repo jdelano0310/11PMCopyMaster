0f79702c578f1f87e9c39fe217847e3ad513228c
Jon Delano <jd31068@hotmail.com>
2024-03-16 05:27:13 -0400

    Add project files.

---
+++ 11PMCopyMaster/Form1.Designer.vb
@@ -0,0 +1,76 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnTest = New Button()
+        lbTestResults = New ListBox()
+        Label1 = New Label()
+        SuspendLayout()
+        ' 
+        ' btnTest
+        ' 
+        btnTest.Location = New Point(3, 12)
+        btnTest.Name = "btnTest"
+        btnTest.Size = New Size(150, 58)
+        btnTest.TabIndex = 0
+        btnTest.Text = "Test"
+        btnTest.UseVisualStyleBackColor = True
+        ' 
+        ' lbTestResults
+        ' 
+        lbTestResults.FormattingEnabled = True
+        lbTestResults.ItemHeight = 25
+        lbTestResults.Location = New Point(180, 34)
+        lbTestResults.Name = "lbTestResults"
+        lbTestResults.Size = New Size(366, 404)
+        lbTestResults.TabIndex = 1
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(187, 3)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(102, 25)
+        Label1.TabIndex = 2
+        Label1.Text = "Test Results"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(566, 450)
+        Controls.Add(Label1)
+        Controls.Add(lbTestResults)
+        Controls.Add(btnTest)
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "11pm Copy Master"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnTest As Button
+    Friend WithEvents lbTestResults As ListBox
+    Friend WithEvents Label1 As Label
+
+End Class
---
+++ 11PMCopyMaster/Form1.vb
@@ -0,0 +1,144 @@
+﻿Imports Microsoft.Office.Interop
+Imports System.IO
+Public Class Form1
+    Dim _xlApp As Excel.Application
+    Dim _xlMasterFile As Excel.Workbook
+    Dim _functionSuccess As Boolean
+    Dim _showLogInListBox As Boolean = False
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ' un comment this to have it run when the app is launched
+        ' call each of the functions that are needed to copy the data
+        If Not OpenMasterFile() Then End
+
+        If Not CopyMasterData() Then End
+
+        CloseMasterFile()
+
+        End
+
+    End Sub
+
+    Private Function OpenMasterFile() As Boolean
+
+        ' open the excel workbook deemed the master file
+        Dim masterFile As String = "F:\Temp\region1.xlsx"
+
+        _functionSuccess = True
+
+        Try
+            WriteToLog($"Opening Master File {masterFile}")
+            _xlApp = New Excel.Application
+            _xlMasterFile = _xlApp.Workbooks.Open(masterFile)
+            WriteToLog("Opened")
+
+        Catch ex As Exception
+            WriteToLog($"Error Opening Master File {masterFile} {ex.Message}")
+            _functionSuccess = False
+
+            If Not _xlApp Is Nothing Then _xlApp.Quit() ' close Excel if it is open
+        End Try
+
+        Return _functionSuccess
+
+    End Function
+
+    Private Sub WriteToLog(writeText As String)
+        ' this sub simply opens a text file for appending
+        ' adds a time staps and whatever the write text is to a log file
+
+        Dim logFile As New IO.StreamWriter(Application.StartupPath & "11pmCopyLog.txt", True)
+
+        logFile.WriteLine($"{Now.ToShortDateString} {Now.ToShortTimeString}: {writeText}")
+
+        If _showLogInListBox Then lbTestResults.Items.Add($"{Now.ToShortDateString}: {writeText}")
+        logFile.Close()
+
+    End Sub
+
+    Private Function CopyMasterData() As Boolean
+
+        ' copy the values of the formulas to a new sheet in the master file
+
+        ' you could move these values to a text file so if
+        ' the master file format changes you don't have to
+        ' modify the code directly
+        Dim masterSheetName As String = "Master"
+        Dim masterCellForNewName As String = "D1"
+        Dim masterFromCopyRange As String = "D3:D5"
+        Dim newSheetCopyToRange As String = "A2:A4"
+
+        Dim xlMasterSheet As Excel.Worksheet = _xlMasterFile.Worksheets(masterSheetName)
+        Dim xlNewSheet As Excel.Worksheet = Nothing
+
+        Dim currentCodeAttempt As String = ""
+
+        _functionSuccess = True
+
+        Try
+            WriteToLog($"Copying Master Sheet Data")
+
+            ' create a new sheet for the data
+            currentCodeAttempt = "Adding new sheet"
+            _xlMasterFile.Worksheets.Add(, xlMasterSheet)
+            xlNewSheet = _xlMasterFile.Worksheets(_xlMasterFile.Worksheets.Count)
+            xlNewSheet.Name = xlMasterSheet.Range(masterCellForNewName).Value
+
+            ' add a title row to the new sheet
+            currentCodeAttempt = "Adding title to new sheet"
+            xlNewSheet.Range("A1").Value = "Totals"
+
+            ' copy the values to the new sheet
+            currentCodeAttempt = "Copying data to the new sheet"
+            xlMasterSheet.Range(masterFromCopyRange).Copy()
+            xlNewSheet.Range(newSheetCopyToRange).PasteSpecial(Excel.XlPasteType.xlPasteValues)
+
+            ' save the master file with the new sheet
+            currentCodeAttempt = "Saving the workbook"
+            _xlMasterFile.Save()
+
+            WriteToLog("All Data Copied")
+
+        Catch ex As Exception
+            WriteToLog($"Error Copying Master Sheet Data during {currentCodeAttempt} {ex.Message}")
+            _functionSuccess = False
+
+        End Try
+
+        Return _functionSuccess
+
+    End Function
+
+    Private Function CloseMasterFile() As Boolean
+
+        ' close the workbook
+        _functionSuccess = True
+
+        Try
+            WriteToLog("Closing Master File")
+            _xlMasterFile.Close()
+            _xlApp.Quit()
+            WriteToLog("Closed")
+        Catch ex As Exception
+            WriteToLog($"Error Closing Master File {ex.Message}")
+            _functionSuccess = False
+        End Try
+
+        Return _functionSuccess
+    End Function
+
+    Private Sub btnTest_Click(sender As Object, e As EventArgs) Handles btnTest.Click
+
+        _showLogInListBox = True
+        lbTestResults.Items.Clear()
+
+        ' call each of the functions that are needed to copy the data
+        If Not OpenMasterFile() Then End
+
+        If Not CopyMasterData() Then End
+
+        CloseMasterFile()
+
+    End Sub
+End Class
